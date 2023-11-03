0b431c08e0df38f682228b24355f641ac6dace77
Clayton Siby <claytonsiby@gmail.com>
2023-11-03 06:30:18 +0200

    Refactor: pick_color function and add color to the Image struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,10 +5,8 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image { hex: [r, g, b | _tail] } = image
-
-    [r, g, b]
+  def pick_color(%Identicon.Image { hex: [r, g, b | _tail] } = image) do
+    %Identicon.Image{image | color: {r, g, b}} # tuple {r,g,b}
   end
 
   def has_input(input) do
